0727c7df0c28b9bb9e34255e548edbb38fe76945
ionosens <pbaizan@ieec.uned.es>
2026-05-17 21:01:32 +0200

    inicial

---
+++ main.py
@@ -0,0 +1,182 @@
+'''
+Copyright (C) 2018  Pablo Baizan
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+'''
+
+import datetime
+import time
+import os
+import string
+from socket import *
+from instruments import Circuit
+from circuitsolver.compdefreader import *
+from circuitsolver.listparser import *
+from httpserver.httpserver import HttpServer
+from eqcom.eqcom import EQCom
+from authenticator.authenticator import Authentication
+from authenticator.session import SessionRegistry
+from circuitsolver.maxfilesregistry import MaxFilesRegistry
+# Anadido para pruebas eliminar client solo
+from authenticator.client import Client, ClientRegistry
+
+
+def main():
+    ##########################################################################
+    # Configuracion Inicial, Valores por defecto
+    ##########################################################################
+    EQ = "localhost"
+    EQPort = 5001  # Visir
+    NumMaxSessions = 30
+    SessionTimeOut = 3600
+    HttpPort = 8080
+    AllowKeepAlive = True
+    ByPassAuth = True
+    ##########################################################################
+    Version = "00.00.02 Beta"
+    Now = str(datetime.datetime.now())
+    print ("******************************************************")
+    print ("*** Visir Measurement Server Version " + Version + " ***")
+    print ("******************************************************")
+    print ("[" + Now + "] Starting server")
+    CurrentPath = os.getcwd()
+    ConfigPath = os.path.join(CurrentPath, "conf")
+    ConfigFile = os.path.join(ConfigPath, "measureserver.conf")
+    Now = str(datetime.datetime.now())
+    print ("[" + Now + "] Read Config File")
+    File = None
+    try:
+        File = open(ConfigFile, "r")
+    except BaseException:
+        # chequeamos que realmente se ha abierto el fichero si no parametros
+        # por defecto y advertencia
+        print ("\t\033[35mWARNING:\033[0m Config File not found," \
+            " default values are used")
+    if File:
+        Line = File.readline()  # leemos la primera linea
+        while Line:
+            if Line[0] != "#":  # Linea comentada, se ignora
+                End = Line.find(" ")
+                if End != -1:
+                    Comand = Line[:End]
+                    Value = Line[End + 1:Line.find("\n")]
+                    if Comand == "EQ":
+                        EQ = Value
+                    elif Comand == "EQPort":
+                        EQPort = int(Value)
+                    elif Comand == "ByPassAuth":
+                        ByPassAuth = bool(Value)
+                    elif Comand == "NumMaxSessions":
+                        NumMaxSessions = int(Value)
+                    elif Comand == "SessionTimeOut":
+                        SessionTimeOut = int(Value)
+                    elif Comand == "AllowKeepAlive":
+                        AllowKeepAlive = bool(Value)
+                    elif Comand == "HttpPort":
+                        HttpPort = int(Value)
+            Line = File.readline()
+    FileComponentsType = os.path.join(ConfigPath, "component.types")
+    componentReader = ComponentDefinitionReader()
+    componentReader.ReadFile(FileComponentsType)
+    ComponentsDefinitions = componentReader.GetDefinitions()
+    ComponentDefinitionRegister = ComponentDefinitionRegistry()
+    ComponentDefinitionRegister.Register()
+    ComponentDefinitionRegister.AddComponentDefinition(ComponentsDefinitions)
+    Now = str(datetime.datetime.now())
+    print ("[" + Now + "] Reading maxlist config")
+    FileMaxList = os.path.join(ConfigPath, "maxlist.conf")
+    MaxFilesComponents = []
+    MaxFilesName = []
+    MaxfilesRegister = MaxFilesRegistry()
+    MaxfilesRegister.Register()
+    # Si no localiza archivo con maxlist error y no merece la pena crear
+    # excepcion
+    File = open(FileMaxList, "r")
+    Line = File.readline()  # leemos la primera linea
+    Parser = ListParser(ComponentsDefinitions)
+    while Line:
+        if Line[0] != "#":  # Linea comentada, se ignora
+            Num = Line.find(".max")
+            if Line[0] == "/" or Line[0] == "\\":
+                Line = Line[1:Num] + ".max"
+            else:
+                Line = Line[0:Num] + ".max"
+            if os.sep == "\\":
+                Line.replace("/", "\\")
+            else:
+                Line = string.replace(Line, "\\", "/")
+            Now = str(datetime.datetime.now())
+            print ("[" + Now + "] Reading maxlist: " + Line)
+            # Line = os.path.normpath(Line) #Solo funcionaba en algunos OS
+            MaxFile = os.path.join(ConfigPath, Line)
+            Parser.ParseFile(MaxFile)
+            MaxFilesName.append(Line)
+            MaxFilesComponents.append(Parser.GetList())
+            MaxfilesRegister.AddMaxFile(Parser.GetList())
+        Line = File.readline()
+    Now = str(datetime.datetime.now())
+    print ("[" + Now + "] Loading module: EQCOM")
+    EQServer = EQCom()
+    Now = str(datetime.datetime.now())
+    print ("[" + Now + "] Registering EQCOM Module")
+    EQServer.Register(EQ, EQPort)
+    Now = str(datetime.datetime.now())
+    print ("[" + Now + "] Sending component list request to equipment server")
+    EQCircuit = Circuit(ComponentsDefinitions)
+    if EQCircuit.Error:
+        print ("\033[31mError:\033[0m " + str(EQCircuit.ComponentList))
+    else:
+        for It in range(0, len(MaxFilesComponents)):
+            Value, Boolean = EQCircuit.MatchIndex(
+                MaxFilesComponents[It], EQCircuit.ComponentList)
+            if not Boolean:
+                print ("\033[35mWARNING:\033[0m Maxlist " +
+                       MaxFilesName[It] +
+                       " is not a subset of the componentlist")
+                for Item in Value:
+                    if Item[0] == -1:
+                        MaxFileFail = MaxFilesComponents[It]
+                        print ("\t - Failed on " + MaxFileFail[Item[-1]].Dump())
+                print ("\033[31mError:\033[0m The returned componentlist is" \
+                    " not a superset of the used maxlists")
+        ComponentDefinitionRegister.AddComponentList(EQCircuit.ComponentList)
+        SessionControl = SessionRegistry()
+        SessionControl.Register(NumMaxSessions, SessionTimeOut)
+        Now = str(datetime.datetime.now())
+        print ("[" + Now + "] Registering Authentication Module")
+        VisirAuthentication = Authentication()
+        VisirAuthentication.Register(
+            SessionControl, AllowKeepAlive, ByPassAuth)
+        # VisirAuthentication.RegisterAuthenticator() #Aqui se registrarian los
+        # diferentes modos de autentificar
+        # Inicializamos los diferentes metodos de autentificacion si ByPassAuth
+        # se lo salta
+        VisirAuthentication.Init()
+        ClientList = ClientRegistry()
+        ClientList.Register()
+        # *******************************************************************
+        # Eliminar la funcion siguiente solo creada en primera
+        # version para pruebas
+        # *******************************************************************
+        TestClient = Client()
+        ClientList.AddClient(TestClient)
+        Now = str(datetime.datetime.now())
+        print ("[" + Now + "] Initialization complete, "
+               "staring to listen for incoming connections")
+        WebServer = HttpServer('0.0.0.0', HttpPort, True)
+        WebServer.Init()
+
+
+if __name__ == "__main__":
+    main()
---
+++ xmlserver/xmlparser.py
@@ -0,0 +1,328 @@
+'''
+Copyright (C) 2018  Pablo Baizan
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+'''
+# Por compatibilid de codigo con el nativo de python le cambio el nombre
+import xml.etree.cElementTree as ET
+
+
+class XMLParser(object):
+    def __init__(self):
+        self.__mProtocol = None
+        self.__mRequest = None
+        self.__mLogin = None
+        self.IsLogin = False
+        self.__mCircuit = []
+        self.__mMultimeter = []
+        self.__mFunctionGenerator = []
+        self.__mOscilloscope = []
+        self.__mDCPower = []
+
+    def ParserRoot(self, XMLStream):
+        Root = ET.fromstring(XMLStream)
+        Iter = Root.iter()
+        for Element in Iter:
+            if Element.tag == "protocol":
+                self.__mProtocol = Element
+            elif Element.tag == "login":
+                self.IsLogin = True
+                self.__mLogin = Element
+            elif Element.tag == "request":
+                self.IsLogin = False
+                self.__mRequest = Element
+            elif Element.tag == "circuit":
+                self.__mCircuit = Element
+            elif Element.tag == "multimeter":
+                self.__mMultimeter.append(Element)
+            elif Element.tag == "functiongenerator":
+                self.__mFunctionGenerator.append(Element)
+            elif Element.tag == "oscilloscope":
+                self.__mOscilloscope.append(Element)
+            elif Element.tag == "dcpower":
+                self.__mDCPower.append(Element)
+
+    def ParserProtocol(self):
+        # retorno valor de version
+        return float(self.__mProtocol.items()[0][1])
+
+    def ParserRequest(self):
+        # return hex(int(self.__mRequest.items()[0][1], 16)) # retorno valor
+        # sesionkey (como int hex, ver si es mas interesante como string)
+        # retorno valor sessionkey (como string, ver si es mas interesante como
+        # int hex)
+        return self.__mRequest.items()[0][1]
+
+    def ParserCircuit(self):
+        Iter = self.__mCircuit.iter()
+        # Lo pasamos como un array por si circuit incluye
+        # mas de un CircuitList
+        CircuitList = []
+        for Element in Iter:
+            if Element.text:
+                CircuitList.append(Element.text)
+        return CircuitList  # retorno array con cicuitlists que hay en circuit
+
+    def ParserMultimeter(self):
+        Multimeter = []
+        for it in self.__mMultimeter:
+            Iter = it.iter()
+            MultimeterDict = {
+                "id": None,
+                "function": None,
+                "resolution": None,
+                "range": None,
+                "autozero": None}
+            for Element in Iter:
+                if Element.tag == "multimeter":
+                    MultimeterDict["id"] = int(Element.items()[0][1])
+                elif Element.tag == "dmm_function":
+                    MultimeterDict["function"] = Element.items()[0][1]
+                elif Element.tag == "dmm_resolution":
+                    MultimeterDict["resolution"] = Element.items()[0][1]
+                elif Element.tag == "dmm_range":
+                    # todo: lo dejo como string ver si tienen que ser int
+                    MultimeterDict["range"] = Element.items()[0][1]
+                elif Element.tag == "dmm_autozero":
+                    # todo: lo dejo como string ver si tienen que ser int
+                    MultimeterDict["autozero"] = Element.items()[0][1]
+            Multimeter.append(MultimeterDict)
+        # retornamos un array con los diccionarios correspondientes a cada uno
+        # de los multimetros
+        return Multimeter
+
+    def ParserFunctionGenerator(self):
+        FunctionGenerator = []
+        for it in self.__mFunctionGenerator:
+            Iter = it.iter()
+            FunctionGeneratorDict = {
+                "id": None,
+                "waveform": None,
+                "frequency": None,
+                "amplitude": None,
+                "offset": None}
+            for Element in Iter:
+                if Element.tag == "functiongenerator":
+                    FunctionGeneratorDict["id"] = int(Element.items()[0][1])
+                elif Element.tag == "fg_waveform":
+                    FunctionGeneratorDict["waveform"] = Element.items()[0][1]
+                elif Element.tag == "fg_frequency":
+                    # todo: lo dejo como string ver si tiene que ser int/float
+                    FunctionGeneratorDict["frequency"] = Element.items()[0][1]
+                elif Element.tag == "fg_amplitude":
+                    # todo: lo dejo como string ver si tienen que ser float
+                    FunctionGeneratorDict["amplitude"] = Element.items()[0][1]
+                elif Element.tag == "fg_offset":
+                    # todo: lo dejo como string ver si tienen que ser int/float
+                    FunctionGeneratorDict["offset"] = Element.items()[0][1]
+            FunctionGenerator.append(FunctionGeneratorDict)
+        # retornamos un array con los diccionarios correspindientes a cada uno
+        # de los generadores de funcion
+        return FunctionGenerator
+
+    def ParserLogin(self):
+        # return int(self.__mLogin.items()[1][1]),
+        # hex(int(self.__mLogin.items()[0][1], 16)) # retorno valor keepalive
+        # en entero y cockie en hex
+        # retorno valor keeplive como entero y cockie como string
+        return int(self.__mLogin.items()[0][1]), self.__mLogin.items()[1][1]
+
+    def ParserOscilloscope(self):
+        Oscilloscope = []
+        for it in self.__mOscilloscope:
+            Iter = it.iter()
+            OscilloscopeDict = {
+                "id": None,
+                "samplerate": None,
+                "refpos": None,
+                "horz_recordlength": None,
+                "chan1_enabled": None,
+                "chan1_coupling": None,
+                "chan1_range": None,
+                "chan1_offset": None,
+                "chan1_attenuation": None,
+                "chan2_enabled": None,
+                "chan2_coupling": None,
+                "chan2_range": None,
+                "chan2_offset": None,
+                "chan2_attenuation": None,
+                "trig_source": None,
+                "trig_slope": None,
+                "trig_coupling": None,
+                "trig_level": None,
+                "trig_mode": None,
+                "trig_timeout": None,
+                "trig_delay": None,
+                "meas1_channel": None,
+                "meas1_selection": None,
+                "meas2_channel": None,
+                "meas2_selection": None,
+                "meas3_channel": None,
+                "meas3_selection": None,
+                "osc_autoscale": None}
+            ChannelEnable = False
+            ChannelCoupling = False
+            ChannelRange = False
+            ChannelOffset = False
+            ChannelAttenuation = False
+            MeasChannel = 0
+            MeasSelection = 0
+            for Element in Iter:
+                if Element.tag == "oscilloscope":
+                    OscilloscopeDict["id"] = Element.items()[0][1]
+                elif Element.tag == "horz_samplerate":
+                    OscilloscopeDict["samplerate"] = Element.items()[0][1]
+                elif Element.tag == "horz_refpos":
+                    OscilloscopeDict["refpos"] = Element.items()[0][1]
+                elif Element.tag == "horz_recordlength":
+                    OscilloscopeDict["horz_recordlength"] = Element.items()[
+                        0][1]
+                elif Element.tag == "chan_enabled":
+                    # miro si es la primer pasada (channel 1) o la segunda
+                    # (channel2)
+                    if not ChannelEnable:
+                        OscilloscopeDict["chan1_enabled"] = Element.items()[
+                            0][1]
+                    else:
+                        OscilloscopeDict["chan2_enabled"] = Element.items()[
+                            0][1]
+                    ChannelEnable = True
+                elif Element.tag == "chan_coupling":
+                    # miro si es la primer pasada (channel 1) o la segunda
+                    # (channel2)
+                    if not ChannelCoupling:
+                        OscilloscopeDict["chan1_coupling"] = Element.items()[
+                            0][1]
+                    else:
+                        OscilloscopeDict["chan2_coupling"] = Element.items()[
+                            0][1]
+                    ChannelCoupling = True
+                elif Element.tag == "chan_range":
+                    # miro si es la primer pasada (channel 1) o la segunda
+                    # (channel2)
+                    if not ChannelRange:
+                        OscilloscopeDict["chan1_range"] = Element.items()[0][1]
+                    else:
+                        OscilloscopeDict["chan2_range"] = Element.items()[0][1]
+                    ChannelRange = True
+                elif Element.tag == "chan_offset":
+                    # miro si es la primer pasada (channel 1) o la segunda
+                    # (channel2)
+                    if not ChannelOffset:
+                        OscilloscopeDict["chan1_offset"] = Element.items()[
+                            0][1]
+                    else:
+                        OscilloscopeDict["chan2_offset"] = Element.items()[
+                            0][1]
+                    ChannelOffset = True
+                elif Element.tag == "chan_attenuation":
+                    # miro si es la primer pasada (channel 1) o la segunda
+                    # (channel2)
+                    if not ChannelAttenuation:
+                        OscilloscopeDict["chan1_attenuation"] = \
+                            Element.items()[0][1]
+                    else:
+                        OscilloscopeDict["chan2_attenuation"] = \
+                            Element.items()[0][1]
+                    ChannelAttenuation = True
+                elif Element.tag == "trig_source":
+                    OscilloscopeDict["trig_source"] = Element.items()[0][1]
+                elif Element.tag == "trig_slope":
+                    OscilloscopeDict["trig_slope"] = Element.items()[0][1]
+                elif Element.tag == "trig_coupling":
+                    OscilloscopeDict["trig_coupling"] = Element.items()[0][1]
+                elif Element.tag == "trig_level":
+                    OscilloscopeDict["trig_level"] = Element.items()[0][1]
+                elif Element.tag == "trig_mode":
+                    OscilloscopeDict["trig_mode"] = Element.items()[0][1]
+                elif Element.tag == "trig_timeout":
+                    OscilloscopeDict["trig_timeout"] = Element.items()[0][1]
+                elif Element.tag == "trig_delay":
+                    OscilloscopeDict["trig_delay"] = Element.items()[0][1]
+                elif Element.tag == "meas_channel":
+                    if MeasChannel == 0:
+                        OscilloscopeDict["meas1_channel"] = Element.items()[
+                            0][1]
+                        MeasChannel += 1
+                    elif MeasChannel == 1:
+                        OscilloscopeDict["meas2_channel"] = Element.items()[
+                            0][1]
+                        MeasChannel += 1
+                    else:
+                        OscilloscopeDict["meas3_channel"] = Element.items()[
+                            0][1]
+                        MeasChannel = 0
+                elif Element.tag == "meas_selection":
+                    if MeasSelection == 0:
+                        OscilloscopeDict["meas1_selection"] = Element.items()[
+                            0][1]
+                        MeasSelection += 1
+                    elif MeasSelection == 1:
+                        OscilloscopeDict["meas2_selection"] = Element.items()[
+                            0][1]
+                        MeasSelection += 1
+                    else:
+                        OscilloscopeDict["meas3_selection"] = Element.items()[
+                            0][1]
+                        MeasSelection = 0
+                elif Element.tag == "osc_autoscale":
+                    OscilloscopeDict["osc_autoscale"] = Element.items()[0][1]
+            Oscilloscope.append(OscilloscopeDict)
+        return Oscilloscope
+
+    def ParserDCPower(self):
+        DCPower = []
+        for it in self.__mDCPower:
+            Iter = it.iter()
+            DCPowerDict = {
+                "id": None,
+                "6V+_dc_voltage": None,
+                "6V+_dc_current": None,
+                "25V+_dc_voltage": None,
+                "25V+_dc_current": None,
+                "25V-_dc_voltage": None,
+                "25V-_dc_current": None}
+            DCOutputVoltage = 0
+            DCOutputCurrent = 0
+            for Element in Iter:
+                if Element.tag == "dcpower":
+                    DCPowerDict["id"] = int(Element.items()[0][1])
+                elif Element.tag == "dc_voltage":
+                    if DCOutputVoltage == 0:
+                        DCPowerDict["6V+_dc_voltage"] = float(
+                            Element.items()[0][1])
+                        DCOutputVoltage += 1
+                    elif DCOutputVoltage == 1:
+                        DCPowerDict["25V+_dc_voltage"] = float(
+                            Element.items()[0][1])
+                        DCOutputVoltage += 1
+                    else:
+                        DCPowerDict["25V-_dc_voltage"] = float(
+                            Element.items()[0][1])
+                        DCOutputVoltage = 0
+                elif Element.tag == "dc_current":
+                    if DCOutputCurrent == 0:
+                        DCPowerDict["6V+_dc_current"] = float(
+                            Element.items()[0][1])
+                        DCOutputCurrent += 1
+                    elif DCOutputCurrent == 1:
+                        DCPowerDict["25V+_dc_current"] = float(
+                            Element.items()[0][1])
+                        DCOutputCurrent += 1
+                    else:
+                        DCPowerDict["25V-_dc_current"] = float(
+                            Element.items()[0][1])
+                        DCOutputCurrent = 0
+            DCPower.append(DCPowerDict)
+        return DCPower
